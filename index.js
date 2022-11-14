4c64a403ac70345e1a433c2a61dd3d07526d648a
moeqaz <usamaqazi98@gmail.com>
2022-11-14 15:34:26 -0500

    update register route

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+//Update the location of the folder for the res.render to use
+app.set('views', path.join(__dirname, 'src/templates/views'))
 
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-    res.render('register', { username: 'moeqaz' })
-})
 
-app.get('/users', (req, res) => {
-    res.send('Users')
+app.get('/', (req, res) => {
+    res.send('Hello World')
 })
 
 // Import function to initialize routes
