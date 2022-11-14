575fe82e3895d93ee0dc28e846770be85d21f023
moeqaz <usamaqazi98@gmail.com>
2022-11-14 15:25:42 -0500

    add auth router

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.render('register', { username: 'moeqaz' })
 })
 
 app.get('/users', (req, res) => {
     res.send('Users')
 })
+
+// Import function to initialize routes
+const initRoutes = require('./src/routes');
+console.log(initRoutes);
+
+// Call the init routes function with the app
+initRoutes(app);
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`)
---
+++ src/routes/auth/index.js
@@ -0,0 +1,12 @@
+const MainAuthRouter = require('express').Router();
+
+MainAuthRouter.route('/register')
+    .get((req, res) => {
+        res.send('Main Auth Router Register')
+    })
+    .post((req, res) => {
+        res.send('Post Main Auth Router')
+    }) 
+
+
+module.exports = MainAuthRouter
